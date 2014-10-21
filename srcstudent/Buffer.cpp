84c69b2ad8b8399444fe1a450521e05e17925c31
user <vincent.chataignier@etu.univ-tours.fr>
2014-10-21 13:57:50 +0200

    first commit

---
+++ srcstudent/Buffer.cpp
@@ -0,0 +1,25 @@
+#include <Buffer.h>
+#include <FastMath.h>
+
+
+void Buffer::DrawLine(const Coord2D p1, const Coord2D p2, const Color c1,
+		const Color c2)
+{
+	// compléter ici
+}
+
+void Buffer::DrawFilledTriangle(const Coord2D p1, const Coord2D p2,
+		const Coord2D p3, const Color c1, const Color c2, const Color c3)
+{
+	// completer ici
+}
+
+void Buffer::DrawPhongTriangle(const Coord2D p1, const Coord2D p2,
+		const Coord2D p3, const Color c1, const Color c2, const Color c3,
+		const Coord3D posi1, const Coord3D posi2, const Coord3D posi3,
+		const Coord3D normal1, const Coord3D normal2, const Coord3D normal3,
+		const AmbientLight & ambientLight, const PointLight & pointLight)
+{
+	// compléter ici
+}
+
