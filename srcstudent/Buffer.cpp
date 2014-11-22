484561896d48314626802878c7d65f119f573a46
Florent Clarret <florent.clarret@gmail.com>
2014-11-22 18:25:52 +0100

    Phong terminé

--- srcstudent/Buffer.cpp
+++ srcstudent/Buffer.cpp
@@ -98,6 +98,7 @@ void Buffer::DrawFilledTriangle(const Coord2D p1, const Coord2D p2,
     }
 }
 
+
 void Buffer::DrawPhongTriangle(const Coord2D p1,
                                const Coord2D p2,
                                const Coord2D p3,
@@ -116,46 +117,94 @@ void Buffer::DrawPhongTriangle(const Coord2D p1,
     Color colors[3] = {c1, c2, c3};
     Coord3D positions3D[3] = {posi1, posi2, posi3};
     Coord3D normals[3] = {normal1, normal2, normal3};
-    Coord2D leftPoint, rightPoint;
-    Coord3D leftPosition, rightPosition;
+    Coord2D pointLeft, pointRight;
 
     scanLineComputer.Init();
     scanLineComputer.Compute(p1, p2, p3);
 
-    for(int y = scanLineComputer.ymin; y <= scanLineComputer.ymax; ++y){
+    for(int y = scanLineComputer.ymin; y < scanLineComputer.ymax; ++y){
         Coord3D normalLeft, normalRight;
         Color colorLeft, colorRight;
+        Coord3D positionLeft, positionRight;
         double distanceRightLeft;
 
-        leftPoint.y = rightPoint.y = y;
-        leftPoint.x = scanLineComputer.left.data[y];
-        rightPoint.x = scanLineComputer.right.data[y];
+        pointLeft.y = pointRight.y = y;
+        pointLeft.x = scanLineComputer.left.data[y];
+        pointRight.x = scanLineComputer.right.data[y];
 
-        distanceRightLeft = leftPoint.Distance(rightPoint);
+        distanceRightLeft = pointLeft.Distance(pointRight);
 
         for(int i = 0; i < 3; ++i){
             double currentLeftWeight = scanLineComputer.leftweight.data[y].data[i];
             double currentRightWeight = scanLineComputer.rightweight.data[y].data[i];
 
             colorRight = colorRight + (colors[i] * currentRightWeight);
-            normalRight += normals[i] * currentRightWeight;
-            rightPosition += positions3D[i] * currentRightWeight;
-            normalLeft += normals[i] * currentLeftWeight;
-            colorLeft = colorLeft + colors[i] * currentLeftWeight;
-            leftPosition += positions3D[i] * currentLeftWeight;
+            colorLeft = colorLeft + (colors[i] * currentLeftWeight);
+            normalRight = normalRight + (normals[i] * currentRightWeight);
+            normalLeft = normalLeft + (normals[i] * currentLeftWeight);
+            positionRight = positionRight + (positions3D[i] * currentRightWeight);
+            positionLeft = positionLeft + (positions3D[i] * currentLeftWeight);
         }
 
-        for(int x = leftPoint.x; x <= rightPoint.x; ++x){
+        for(int x = pointLeft.x; x <= pointRight.x; ++x){
             Coord2D currentPoint(x, y);
-            double distanceLeftCurrent = leftPoint.Distance(currentPoint);
-            double w1 = 1 - (distanceLeftCurrent / distanceRightLeft), w2 = 1 - w1;
+            double w1 = 1 - (pointLeft.Distance(currentPoint) / distanceRightLeft), w2 = 1 - w1;
 
             SetPoint(currentPoint,
                      (colorLeft * w1 + colorRight * w2) *
                      (ambientLight.ambientColor + pointLight.GetColor(
-                                                         (leftPosition * w1) + (rightPosition * w2),
+                                                         (positionLeft * w1) + (positionRight * w2),
                                                          (normalLeft * w1) + (normalRight * w2))));
         }
     }
-}
+}/*
 
+void Buffer::DrawPhongTriangle(const Coord2D p1, const Coord2D p2,
+const Coord2D p3, const Color c1, const Color c2, const Color c3,
+const Coord3D posi1, const Coord3D posi2, const Coord3D posi3,
+const Coord3D normal1, const Coord3D normal2, const Coord3D normal3,
+const AmbientLight & ambientLight, const PointLight & pointLight)
+{
+scanLineComputer.Init();
+scanLineComputer.Compute( p1, p2, p3 );
+Color colorLeft, colorRight;
+Coord3D current3D, leftPosi, rightPosi, normalLeft, normalRight;
+// Parcours de chaque ligne
+for( int i = scanLineComputer.ymin; i < scanLineComputer.ymax; ++i )
+{
+Coord2D pointLeft(scanLineComputer.left.data[i], i);
+Coord2D pointRight(scanLineComputer.right.data[i], i);
+double distance = pointLeft.Distance(pointRight);
+normalLeft = normal1 * scanLineComputer.leftweight.data[i].data[0] +
+normal2 * scanLineComputer.leftweight.data[i].data[1] +
+normal3 * scanLineComputer.leftweight.data[i].data[2];
+normalRight = normal1 * scanLineComputer.rightweight.data[i].data[0] +
+normal2 * scanLineComputer.rightweight.data[i].data[1] +
+normal3 * scanLineComputer.rightweight.data[i].data[2];
+colorLeft = c1 * scanLineComputer.leftweight.data[i].data[0] +
+c2 * scanLineComputer.leftweight.data[i].data[1] +
+c3 * scanLineComputer.leftweight.data[i].data[2];
+colorRight = c1 * scanLineComputer.rightweight.data[i].data[0] +
+c2 * scanLineComputer.rightweight.data[i].data[1] +
+c3 * scanLineComputer.rightweight.data[i].data[2];
+leftPosi = posi1 * scanLineComputer.leftweight.data[i].data[0] +
+posi2 * scanLineComputer.leftweight.data[i].data[1] +
+posi3 * scanLineComputer.leftweight.data[i].data[2];
+rightPosi = posi1 * scanLineComputer.rightweight.data[i].data[0] +
+posi2 * scanLineComputer.rightweight.data[i].data[1] +
+posi3 * scanLineComputer.rightweight.data[i].data[2];
+// Parcours et dessin de tous les points sur la ligne courante
+for(int j = scanLineComputer.left.data[i]; j <= scanLineComputer.right.data[i]; ++j)
+{
+Coord2D current2D(j, i);
+double w1 = 1.0 - (pointLeft.Distance(current2D) / distance);
+double w2 = 1.0 - w1;
+Coord3D posiCurrent = leftPosi * w1 + rightPosi * w2;
+Coord3D normalCurrent = normalLeft * w1 + normalRight * w2;
+Color diffuseColor = pointLight.GetColor(posiCurrent, normalCurrent);
+Color pointColor = colorLeft * w1 + colorRight * w2;
+SetPoint(current2D, (ambientLight.ambientColor + diffuseColor) * pointColor);
+}
+}
+}
+*/
